cca5956e523418b7330873146ad9f8cb703decdd
papasnoopie <bygeoffreyouma@gmail.com>
2023-08-26 00:04:55 -0700

    Using Puppet, create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/school':
-	ensure => 'file',
-	content => 'I love puppet',
-	mode => 744,
-	owner => 'www-data',
-	group => 'www-data',
+  ensure  => 'file',
+  content => 'I love puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
