94267ff005e51e880320403e69e5af80eae87090
papaskripto <bygeoffreyouma@gmail.com>
2023-10-07 02:46:57 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Python script
-Uses an API for a given employee ID to return information
-about their TODO list progress
-"""
+""" Returns information about employee TODO list progress."""
+
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    user_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(user_id), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(user_id), verify=False).json()
 
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user.get('name'), len(completed_tasks), len(todo)))
-
-    for task in completed_tasks:
-        print("\t {}".format(task))
+if __name__ == "__main__":
+	user_id = argv[1]
+	user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+		format(user_id), verify=False).json()
+	todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+		format(user_id), verify=False).json()
+	number_of_done_tasks = []
+	for task in todo:
+		if task.get("completed") is True:
+			number_of_done_tasks.append(task.get("title"))
+	print("Employee {} is done with tasks ({}/{}):".
+		format(user.get("name"), len(number_of_done_tasks), len(todo)))
+	for task in number_of_done_tasks:
+		print("\t {}".format(task))
