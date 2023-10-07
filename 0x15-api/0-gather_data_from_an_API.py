c483250b4e88ae3151ecb258fad3a0e5dbb97d57
papaskripto <bygeoffreyouma@gmail.com>
2023-10-07 03:07:42 +0000

    Gather info from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,15 +7,15 @@ from sys import argv
 
 if __name__ == "__main__":
 	employee_id = argv[1]
-	employee_name = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-		format(employee_id), verify=False).json()
-	total_number_of_tasks = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-		format(employee_id), verify=False).json()
+	employee_name = requests.get("https://jsonplaceholder.typicode.com\
+			/users/{}".format(employee_id), verify=False).json()
+	total_number_of_tasks = requests.get("https://jsonplaceholder.typicode.com\
+			/todos?userId={}".format(employee_id), verify=False).json()
 	number_of_done_tasks = []
 	for task in total_number_of_tasks:
 		if task.get("completed") is True:
 			number_of_done_tasks.append(task.get("title"))
-	print("Employee {} is done with tasks ({}/{}):".
-		format(employee_name.get("name"), len(number_of_done_tasks), len(total_number_of_tasks)))
+	print("Employee {} is done with tasks ({}/{}):".format(employee_name.get\
+			("name"), len(number_of_done_tasks), len(total_number_of_tasks)))
 	for task in number_of_done_tasks:
 		print("\t {}".format(task))
