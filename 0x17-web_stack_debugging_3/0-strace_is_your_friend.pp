86e54f06c4a95c91e2e94619787cf702043cc093
papaskripto <bygeoffreyouma@gmail.com>
2023-10-14 16:13:10 -0700

    pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Fix Apache server internal error
-exec { 'fix 500 error':
-  command => 'sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/h\
-tml/wp-settings.php',
-  path    => '/bin'
-}
+
+exec { 'fix-apache-error' : 
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'}
